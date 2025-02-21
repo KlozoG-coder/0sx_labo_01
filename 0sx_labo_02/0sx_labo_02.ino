9a487fef22f115ec9011a21d4cfc643291fc70d9
KlozoG-coder <jeanlouiskoudjonou@gmail.com>
2025-02-21 11:40:38 -0500

    Mon deuxieme Labo

--- 0sx_labo_02/0sx_labo_02.ino
+++ 0sx_labo_02/0sx_labo_02.ino
@@ -1,12 +1,12 @@
 // Déclaration des broches
-const int potentiometrePin = A1;  // Broche du potentiomètre
-const int boutonPin = 2;          // Broche du bouton
-const int ledPins[] = {8, 9, 10, 11};  // Broches des DELs
+const int potentiometrePin = A1;  
+const int boutonPin = 2;       
+const int ledPins[] = {8, 9, 10, 11}; 
 
 // Variables
-int valeurPotentiometre = 0;  // Valeur lue du potentiomètre
-int valeurMapped = 0;         // Valeur mappée de 0 à 20
-bool boutonAppuye = false;    // État du bouton
+int valeurPotentiometre = 0;  
+int valeurMapped = 0;         
+bool boutonAppuye = false;    
 
 void setup() {
   // Initialisation des broches
@@ -26,29 +26,29 @@ void loop() {
 
   // Contrôle des DELs en fonction de la valeur mappée
   if (valeurMapped <= 5) {  // 0 - 25%
-    digitalWrite(ledPins[0], HIGH);
+    digitalWrite(ledPins[0], HIGH);  //  1ère DEL allumée
     digitalWrite(ledPins[1], LOW);
     digitalWrite(ledPins[2], LOW);
     digitalWrite(ledPins[3], LOW);
   } else if (valeurMapped <= 10) {  // 25 - 50%
-    digitalWrite(ledPins[0], HIGH);
+    digitalWrite(ledPins[0], HIGH);  // Allumage des 2 premières DELs
     digitalWrite(ledPins[1], HIGH);
     digitalWrite(ledPins[2], LOW);
     digitalWrite(ledPins[3], LOW);
   } else if (valeurMapped <= 15) {  // 50 - 75%
-    digitalWrite(ledPins[0], HIGH);
+    digitalWrite(ledPins[0], HIGH);  // Allumage des 3 premières DELs
     digitalWrite(ledPins[1], HIGH);
     digitalWrite(ledPins[2], HIGH);
     digitalWrite(ledPins[3], LOW);
   } else {  // 75 - 100%
-    digitalWrite(ledPins[0], HIGH);
+    digitalWrite(ledPins[0], HIGH);  // Allumage des 4 DELs
     digitalWrite(ledPins[1], HIGH);
     digitalWrite(ledPins[2], HIGH);
     digitalWrite(ledPins[3], HIGH);
   }
 
   // Lecture de l'état du bouton
-  if (digitalRead(boutonPin) {
+  if (digitalRead(boutonPin) == HIGH) {
     if (!boutonAppuye) {
       boutonAppuye = true;
       afficherBarreProgression(valeurMapped);
@@ -60,11 +60,11 @@ void loop() {
 
 // Fonction pour afficher la barre de progression
 void afficherBarreProgression(int valeur) {
-  int pourcentage = valeur * 5;  // Convertir en pourcentage (0-20 -> 0-100)
+  int pourcentage = valeur * 5;  // Convertir en pourcentage 
   Serial.print("[");
   for (int i = 0; i < 20; i++) {
     if (i < valeur) {
-      Serial.print("#");
+      Serial.print("#");  // L'avant-dernier chiffre de mon numero etudiant est pair : j'utilise #
     } else {
       Serial.print(".");
     }
